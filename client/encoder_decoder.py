371047e59fe8e45816243725d0de7853ce680c02
CiaranCostello <costelci@tcd.ie>
2016-04-08 15:00:39 +0100

    added files
    Signed-off-by: CiaranCostello <costelci@tcd.ie>

---
+++ client/encoder_decoder.py
@@ -0,0 +1,112 @@
+import pickle, argparse, Crypto, tornado
+from tornado import httpclient
+import requests
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Hash import MD5
+from cryptography.fernet import Fernet
+
+#key size in bits
+KEY_LENGTH = 1024
+
+def sharedFlags(subparser):
+	required = subparser.add_argument_group('required arguments')
+	required.add_argument('-c', '--config', required=True, help='yaml file describing the application')
+	required.add_argument('-m', '--manager', required=True, help='sepcify the host and port of the docker manager eg 127.0.0.1:3000')
+	subparser.add_argument('-q', '--quiet', required=False, help='Supress messages from the script')
+
+#allows us to specify the command line arguments that the script must recieve in order to execute
+def clargs():
+	main_parser = argparse.ArgumentParser(description='a script to get a symetric key from an address or decode a file using a symetric key')
+	
+	sub_parser = main_parser.add_subparsers(dest='cmd')
+	sub_parser.required = True
+	#sub commands for the program
+	init = sub_parser.add_parser('init', help='initialise in this directory. Creates and saves private and public keys')
+	decrypt = sub_parser.add_parser('decrypt', help='decrypts a file with a symetric key')
+	decrypt.add_argument('-f', '--fileName', required=True, help='file name to decrypt')
+	getKey = sub_parser.add_parser('getKey', help='Requests a key from a web server')
+	getKey.add_argument('-a', '--address', required=True, help='Address of web server to request the key from')
+	getKey.add_argument('-u', '--username', required=True, help='Username')
+	getKey.add_argument('-p', '--password', required=True, help='Password')
+	pkey = sub_parser.add_parser('pkey', help='Prints out public key')
+
+	return main_parser.parse_args()
+
+
+class crypto():
+	def __init__(self, generateKey=False):
+		#get public and private key
+		if generateKey:
+			random_gen = Random.new().read
+			self.keypair = RSA.generate(KEY_LENGTH, random_gen)
+			self.init()
+		else:
+			fh = open('keypair', 'rb')
+			self.keypair = RSA.importKey(fh.read())
+
+	def init(self):
+		#print public key
+		print('Public Key: '+str(self.keypair.publickey()))
+		#save keypair as file
+		fh = open('keypair', 'wb')
+		fh.write(self.keypair.exportKey())
+
+	def decrypt(self, filename):
+		#get symmetric key
+		fh = open('sym_key', 'rb')
+		key = fh.read()
+		#open file
+		fh = open(filename, 'rb')
+		encrypted_file = fh.read()
+		#decrypt file
+		cipher_suite = Fernet(key)
+		decrypted_file = cipher_suite.decrypt(encrypted_file)
+		#write decrypted file
+		fh = open(filename, 'wb')
+		fh.write(decrypted_file)
+
+	def getKey(self, address, username, password):
+		#generate public key
+		public_key = self.keypair.publickey()
+		client = tornado.httpclient.HTTPClient()
+		#get servers public key
+		try:
+			server_pubkey = RSA.importKey(client.fetch(address+'/pubkey').body)
+		except httpclient.HTTPError as e:
+			print("Error: " + str(e))
+		#encrypt password and username
+		e_p = server_pubkey.encrypt(password.encode('utf-8'), 32)[0]
+		e_u = server_pubkey.encrypt(username.encode('utf-8'), 32)[0]
+		#send public key to handler to request symmetric key for bucket
+		body_dict = {'pkey':pickle.dumps(public_key), 'password':e_p, 'username':e_u}
+		body = pickle.dumps(body_dict)
+		request = tornado.httpclient.HTTPRequest(method='POST', url=address+'/key', body=body)
+		try:
+			p = client.fetch(request)
+		except httpclient.HTTPError as e:
+			print("Error: "+ str(e))
+		client.close()
+		encrypted_key = pickle.loads(p.body)
+		#decrypt response using private key
+		key = self.keypair.decrypt(encrypted_key)
+
+		#save key as file
+		fh = open('sym_key', 'wb')
+		fh.write(key)
+
+	#prints out the public key
+	def getPKey(self):
+		public_key = self.keypair.publickey()
+		print(public_key.exportKey())
+
+if __name__ == '__main__':
+	args = clargs()
+	if(args.cmd == 'init'):
+		crypto(generateKey=True).init()
+	elif(args.cmd == 'decrypt'):
+		crypto().decrypt(args.fileName)
+	elif(args.cmd == 'getKey'):
+		crypto().getKey(args.address, args.username, args.password,)
+	elif(args.cmd == 'pkey'):
+		crypto().getPKey()
---
+++ server/upload_server.py
@@ -0,0 +1,94 @@
+import tornado, pickle
+import tornado.ioloop
+import tornado.web
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Hash import MD5
+import os, uuid
+from os import listdir
+from cryptography.fernet import Fernet
+
+__UPLOADS__ = "uploads/"
+#dictionary with user name:password 
+users = {'Ciaran':'I', 'Irwin':'poo'}
+#key size in bits
+KEY_LENGTH = 1024
+
+#provides form and links to uploaded files
+class Userform(tornado.web.RequestHandler):
+    def get(self):the
+        files = listdir(__UPLOADS__)
+        self.render("uploadform.html", files=files)
+
+#handles uploaded files 
+class Upload(tornado.web.RequestHandler):
+    def post(self):
+        fileinfo = self.request.files['filearg'][0]
+        print("fileinfo is" + str(fileinfo))
+        fname = fileinfo['filename']
+        fh = open(__UPLOADS__ + fname, 'wb')
+        #encrypt file
+        cipher_suite = Fernet(key)
+        encryptedFile = cipher_suite.encrypt(fileinfo['body'])
+        #store at address specified by __UPLOADS
+        fh.write(encryptedFile)
+        self.finish(fname + " is uploaded!! Check %s folder" %__UPLOADS__)
+
+#handles requests for symmetric key from client
+class KeyRequests(tornado.web.RequestHandler):
+    def post(self):
+        pickled_dict = self.request.body
+        body_dict = pickle.loads(pickled_dict)
+        pubkey = pickle.loads(body_dict['pkey'])
+        #decrypt password and username from body
+        client_username = asym_key.decrypt(body_dict['username']).decode('utf-8')
+        client_password = asym_key.decrypt(body_dict['password']).decode('utf-8')
+        #if the username and password are in the users return key for bucket
+        if users[client_username] is client_password:
+            print('encrypting key')
+            encrypted_key = pubkey.encrypt(key, 32)[0]
+            p = pickle.dumps(encrypted_key)
+            self.write(p)
+        else:
+            print('Public key not in group')
+
+#return public key
+class pubkey(tornado.web.RequestHandler):
+    def get(self):
+        pubkey = asym_key.publickey()
+        self.write(pubkey.exportKey())
+
+application = tornado.web.Application([
+        (r"/", Userform),
+        (r"/upload", Upload),
+        (r"/files/(.*)",tornado.web.StaticFileHandler, {"path": r"./uploads"},),
+        (r"/key", KeyRequests),
+        (r"/pubkey", pubkey),
+        ], debug=True)
+
+#generate or load a symmetric key
+def getKey():
+    #if there is no key then generate one
+    if "encryptionkey" not in listdir("."):
+        k = Fernet.generate_key()
+        fh = open("encryptionkey", 'wb')
+        fh.write(k)
+    #otherwise load it from the file
+    else:
+        fh = open("encryptionkey", 'rb')
+        k = fh.read()
+    return k
+
+#generate asymmetric public/private key pair
+def genAsymKey():
+    random_gen = Random.new().read
+    return RSA.generate(KEY_LENGTH, random_gen)
+
+#get key as global variable
+key = getKey()
+asym_key = genAsymKey()
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
